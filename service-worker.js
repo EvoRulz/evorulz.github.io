7387fab8813279a2f8fab6375637cb406c520379
EvoRulz <evo93453@gmail.com>
2026-04-11 05:36:36 +1000

    sync index and bump cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "habit-tracker-v285";
+const CACHE = "habit-tracker-v286";
 
 const ASSETS = [
   "./",
@@ -45,6 +45,7 @@ self.addEventListener("fetch", e => {
     })
   );
 });
+
 
 
 
