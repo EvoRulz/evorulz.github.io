b34895b102f54b75f2b2b3e2f947d708e451a081
EvoRulz <evo93453@gmail.com>
2026-04-26 07:21:23 +1000

    v712

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "habit-tracker-v711";
+const CACHE = "habit-tracker-v712";
 
 const ASSETS = [
   "./",
@@ -82,6 +82,7 @@ self.addEventListener("fetch", e => {
     })
   );
 });
+
 
 
 
